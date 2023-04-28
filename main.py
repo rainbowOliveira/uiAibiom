0fb393d2f0916836f0d7bb0d6660f6e5e2d12854
lucaralves <1201456@isep.ipp.pt>
2023-04-28 18:16:40 +0100

    Leitura das imagens que se encontram na diretoria 'DataSets'.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("oi")
+import itk
+import os
+from os import listdir
+
+# Obtêm-se a diretoria onde se encontram todos os datasets.
+project_dir = os.getcwd()
+diretoriaDataSets = os.path.join(project_dir, "DataSets")
+
+# ITK Image type.
+Dimension = 3
+PixelType = itk.US
+ImageType = itk.Image[PixelType, Dimension]
+
+# ITK Image reader.
+reader = itk.ImageFileReader[ImageType].New()
+
+diretoriasDataSets = []
+dataSets = []
+
+# Carregam-se as diretorias de todos os datasets.
+for diretoriaDataSet in listdir(diretoriaDataSets):
+    diretoriasDataSets.append(os.path.join(diretoriaDataSets, diretoriaDataSet))
+print(diretoriasDataSets)
+
+# Carregam-se todos os datasets.
+for diretoriaDataSet in diretoriasDataSets:
+    reader.SetFileName(diretoriaDataSet)
+    reader.Update()
+    dataSets.append(reader.GetOutput())
+print(dataSets)
