6fb52c98e247debafd3c7bd222ef53cf412c0bcf
lucaralves <1201456@isep.ipp.pt>
2023-04-29 00:23:09 +0100

    Display VTK file

--- main.py
+++ main.py
@@ -3,80 +3,113 @@ import vtk
 import os
 from os import listdir
 
+# Estrutura do projeto.
+project_dir = os.getcwd()
+DiretoriaDataSets = os.path.join(project_dir, "DataSets")
+DiretoriaItkOutput = os.path.join(project_dir, "ItkOutput")
+DiretoriasDataSets = []
+
 # ITK Image type.
 Dimension = 3
 PixelType = itk.US
 ImageType = itk.Image[PixelType, Dimension]
 
 # ITK Image reader.
-reader = itk.ImageFileReader[ImageType].New()
+itkReader = itk.ImageFileReader[ImageType].New()
+# ITK Image writer.
+writer = itk.ImageFileWriter[ImageType].New()
 # VTK Image reader.
-reader1 = vtk.vtkDataSetReader()
+vtkReader = vtk.vtkStructuredPointsReader()
 
-diretoriasDataSets = []
+# ITK filters.
+binaryThreshold = itk.BinaryThresholdImageFilter[ImageType, ImageType].New()
+
+# VTK colours.
+colors = vtk.vtkNamedColors()
+# VTK mapper.
+mapper = vtk.vtkDataSetMapper()
+# VTK actor.
+actor = vtk.vtkActor()
+# VTK renderer.
+renderer = vtk.vtkRenderer()
+# VTK renderer_window.
+renderer_window = vtk.vtkRenderWindow()
+# VTK renderer_window_interactor.
+interactor = vtk.vtkRenderWindowInteractor()
+
+# Lista que armazena os itk datasets.
 dataSets = []
 
 def carregarDiretoriasDataSets():
 
-    project_dir = os.getcwd()
-    diretoriaDataSets = os.path.join(project_dir, "DataSets")
     # Carregam-se as diretorias de todos os datasets.
-    for diretoriaDataSet in listdir(diretoriaDataSets):
-        diretoriasDataSets.append(os.path.join(diretoriaDataSets, diretoriaDataSet))
-    print(diretoriasDataSets)
+    for diretoriaDataSet in listdir(DiretoriaDataSets):
+        DiretoriasDataSets.append(os.path.join(DiretoriaDataSets, diretoriaDataSet))
+    print(DiretoriasDataSets)
 
 def carregarDataSets():
 
     # Carregam-se todos os datasets.
-    for diretoriaDataSet in diretoriasDataSets:
-        reader.SetFileName(diretoriaDataSet)
-        reader.Update()
-        dataSets.append(reader.GetOutput())
+    for diretoriaDataSet in DiretoriasDataSets:
+        itkReader.SetFileName(diretoriaDataSet)
+        itkReader.Update()
+        dataSets.append(itkReader.GetOutput())
     print(dataSets)
+
+def binaryThresholdFun(itkImage, label):
+
+    outsideValue = 0
+    insideValue = 1
+
+    binaryThreshold.SetLowerThreshold(label)
+    binaryThreshold.SetUpperThreshold(label)
+    binaryThreshold.SetOutsideValue(outsideValue)
+    binaryThreshold.SetInsideValue(insideValue)
+    binaryThreshold.SetInput(itkImage)
+    binaryThreshold.Update()
+
+def writeItkImage(itkImage):
+
+    writer.SetInput(itkImage)
+    writer.SetFileName(os.path.join(DiretoriaItkOutput, "output.vtk"))
+    writer.Update()
 
 def displayVtkFile(vtkDir):
 
-    colors = vtk.vtkNamedColors()
-
     # Read the source file.
-    reader1.SetFileName(vtkDir)
-    reader1.ReadAllScalarsOn()  # Activate the reading of all scalars
-    reader1.ReadAllVectorsOn()  # Activate the reading of all vectors
-    reader1.ReadAllTensorsOn()  # Activate the reading of all tensors
-    reader1.Update()  # Needed because of GetScalarRange
-    output = reader1.GetOutput()
+    vtkReader.SetFileName(vtkDir)
+    # vtkReader.ReadAllScalarsOn()  # Activate the reading of all scalars
+    # vtkReader.ReadAllVectorsOn()  # Activate the reading of all vectors
+    # vtkReader.ReadAllTensorsOn()  # Activate the reading of all tensors
+    vtkReader.Update()  # Needed because of GetScalarRange
+    output = vtkReader.GetOutput()
     scalar_range = output.GetScalarRange()
 
-    # Create the mapper that corresponds the objects of the vtk.vtk file
-    mapper = vtk.vtkDataSetMapper()
+    # Create the mapper that corresponds the objects of the vtk file
     mapper.SetInputData(output)
     mapper.SetScalarRange(scalar_range)
     # mapper.ScalarVisibilityOff()
 
     # Create the Actor
-    actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     actor.GetProperty().EdgeVisibilityOn()
     actor.GetProperty().SetLineWidth(2.0)
-    actor.GetProperty().SetColor(colors.GetColor3d("MistyRose"))
+    # actor.GetProperty().SetColor(colors.GetColor3d("MistyRose"))
 
-    backface = vtk.vtkProperty()
-    backface.SetColor(colors.GetColor3d('Tomato'))
-    actor.SetBackfaceProperty(backface)
+    # backface = vtk.vtkProperty()
+    # backface.SetColor(colors.GetColor3d('Tomato'))
+    # actor.SetBackfaceProperty(backface)
 
     # Create the Renderer
-    renderer = vtk.vtkRenderer()
     renderer.AddActor(actor)
     renderer.SetBackground(colors.GetColor3d('Wheat'))
 
     # Create the RendererWindow
-    renderer_window = vtk.vtkRenderWindow()
     renderer_window.SetSize(640, 480)
     renderer_window.AddRenderer(renderer)
     renderer_window.SetWindowName('ReadVtkFile')
 
     # Create the RendererWindowInteractor and display the vtk_file
-    interactor = vtk.vtkRenderWindowInteractor()
     interactor.SetRenderWindow(renderer_window)
     interactor.Initialize()
     interactor.Start()
@@ -85,4 +118,6 @@ if __name__ == "__main__":
 
     carregarDiretoriasDataSets()
     carregarDataSets()
-    displayVtkFile(diretoriasDataSets[0])
+    binaryThresholdFun(dataSets[0], 2)
+    writeItkImage(binaryThreshold.GetOutput())
+    displayVtkFile(os.path.join(DiretoriaItkOutput, "output.vtk"))
